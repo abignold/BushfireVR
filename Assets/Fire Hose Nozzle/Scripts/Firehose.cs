36f779bcbd27fd58005f0b62d03eee0640ff87c7
Adam Bignold <none@none>
2019-06-17 14:56:55 +1000

    Added water particles to fire hose gun

---
+++ Assets/Fire Hose Nozzle/Scripts/Firehose.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Firehose : MonoBehaviour
+{
+
+    private ParticleSystem particleSystem;
+    public int emitValue = 5;
+    private Interactable interactable;
+    private SteamVR_Action_Boolean pinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
+    private SteamVR_Action_Boolean grabAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (interactable.attachedToHand)
+        {
+            SteamVR_Input_Sources hand = interactable.attachedToHand.handType;
+            bool isPinch = pinchAction.GetState(hand);
+            bool isGrab = grabAction.GetState(hand);
+            bool isBoth = isGrab && isPinch;
+
+            if (isBoth)
+            {
+                particleSystem.Emit(emitValue);
+            }
+        }
+
+        
+    }
+}

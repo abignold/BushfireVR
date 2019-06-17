43eb1af1899564141a6b2dabd63694b66155524c
Adam Bignold <none@none>
2019-06-17 21:52:45 +1000

    Added water interactions with fire. Added sample scene for ground fire and firehose

--- Assets/Fire Hose Nozzle/Scripts/Firehose.cs
+++ Assets/Fire Hose Nozzle/Scripts/Firehose.cs
@@ -8,7 +8,9 @@ public class Firehose : MonoBehaviour
 {
 
     private ParticleSystem particleSystem;
+    private List<ParticleCollisionEvent> collisionEvents;
     public int emitValue = 5;
+    public float sizeDecrease = 0.99f;
     private Interactable interactable;
     private SteamVR_Action_Boolean pinchAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
     private SteamVR_Action_Boolean grabAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
@@ -19,6 +21,7 @@ public class Firehose : MonoBehaviour
     {
         interactable = GetComponent<Interactable>();
         particleSystem = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
     }
 
     // Update is called once per frame
@@ -40,4 +43,63 @@ public class Firehose : MonoBehaviour
 
         
     }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+
+        other.SendMessageUpwards("WaterCollision", numCollisionEvents, SendMessageOptions.DontRequireReceiver);
+
+
+        //int i = 0;
+
+        //while (i < numCollisionEvents)
+        //{
+        //    if (other.name != "Ground")
+        //    {
+        //        other.SendMessageUpwards("WaterCollision");
+        //        //other.transform.localScale = Vector3.Scale(other.transform.localScale, new Vector3(sizeDecrease, sizeDecrease, sizeDecrease));
+        //        //Vector3.Scale(other.transform.localScale, new Vector3(sizeDecrease, sizeDecrease, sizeDecrease));
+        //        Debug.Log(other.name);
+        //    }
+        //    i++;
+        //}
+    }
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("--FIRECOLLISION2");
+    //}
+
+
+    //void OnParticleTrigger()
+    //{
+    //    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+    //    List<ParticleSystem.Particle> exit = new List<ParticleSystem.Particle>();
+    //    // get the particles which matched the trigger conditions this frame
+    //    int numEnter = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+    //    int numExit = particleSystem.GetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+
+    //    // iterate through the particles which entered the trigger and make them red
+    //    for (int i = 0; i < numEnter; i++)
+    //    {
+    //        ParticleSystem.Particle p = enter[i];
+
+    //        p.size = 10f;
+    //        enter[i] = p;
+    //    }
+
+    //    // iterate through the particles which exited the trigger and make them green
+    //    for (int i = 0; i < numExit; i++)
+    //    {
+    //        ParticleSystem.Particle p = exit[i];
+    //        p.size = 10f;
+    //        exit[i] = p;
+    //    }
+
+    //    // re-assign the modified particles back into the particle system
+    //    particleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+    //    particleSystem.SetTriggerParticles(ParticleSystemTriggerEventType.Exit, exit);
+    //}
+
 }
---
+++ Assets/Items/Fire/GroundFire.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFire : MonoBehaviour
+{
+
+    public int health = 500;
+    public int regenPerSecond = 1;
+    public bool isFireStopped = false;
+    private float originalLightIntensity;
+    private float resistance = 15;
+    private Vector3 damageModifier;
+    private float lightIntensityModifier;
+    private Vector3 originalScale;
+    private Light lightSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        lightSource = GetComponentInChildren<Light>();
+        originalScale = transform.localScale;
+        originalLightIntensity = lightSource.intensity;
+        damageModifier.x = originalScale.x / health;
+        damageModifier.y = originalScale.y / health;
+        damageModifier.z = originalScale.z / health;
+        lightIntensityModifier = originalLightIntensity / health;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFireStopped)
+        {
+            return;
+        }
+        if (transform.localScale.x < (originalScale.x * 0.10))
+        {
+            //Destroy(this.gameObject);
+            this.EndFire();
+            return;
+        }
+        if (transform.localScale.x <= originalScale.x)
+        {
+            transform.localScale += new Vector3(regenPerSecond * damageModifier.x * Time.deltaTime, regenPerSecond * damageModifier.x * Time.deltaTime, regenPerSecond * damageModifier.x * Time.deltaTime);
+            lightSource.intensity += regenPerSecond * lightIntensityModifier * Time.deltaTime;
+        }
+    }
+
+    void WaterCollision(float damage)
+    {
+        damage /= resistance;
+        transform.localScale -= new Vector3((damage * damageModifier.x), (damage * damageModifier.y), (damage * damageModifier.z));
+        lightSource.intensity -= damage * lightIntensityModifier;
+    }
+
+    void EndFire()
+    {
+        ParticleSystem[] particleSystemArray = this.GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem particleSystem in particleSystemArray)
+        {
+            particleSystem.Stop();
+        }
+
+        lightSource.enabled = false;
+
+        isFireStopped = true;
+    }
+
+    void StartFire()
+    {
+        ParticleSystem[] particleSystemArray = this.GetComponents<ParticleSystem>();
+        foreach (ParticleSystem particleSystem in particleSystemArray)
+        {
+            particleSystem.Play();
+        }
+        lightSource.enabled = true;
+        isFireStopped = false;
+    }
+}
